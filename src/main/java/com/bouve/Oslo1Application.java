02237e20932c4125bc51f6b998cd523f22d101e9
Rod Johnson <johnsonroda@gmail.com>
2018-03-05 17:02:03 +0000

    Initial commit from Atomist

---
+++ src/main/java/com/bouve/Oslo1Application.java
@@ -0,0 +1,12 @@
+package com.bouve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oslo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oslo1Application.class, args);
+	}
+}
